7e2cd6a24e703ffb7bed4105adeffa738c0666f0
takuya <cdwbh564@yahoo.co.jp>
2024-09-11 21:20:13 +0900

    create.phpファイルを作成。POSTされた読書情報を取得と、データベースに接続する処理を追加。また、initialize_reviews_table.phpからデータベースに接続するコードを、mysqli.phpファイルに分離。 データベースに接続するコードを、create.phpと共有する為。

--- reviews/create.php
+++ reviews/create.php
@@ -1,1 +1,23 @@
 <?php
+
+require_once __DIR__ . '/lib/mysqli.php';
+
+$list = dbConnect();
+// HTTPメソッドがPOSTか判定
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+  $review = [
+      'title' => $_POST['title'],
+      'author' => $_POST['author'],
+      'status' => $_POST['status'],
+      'score' => $_POST['score'],
+      'summary' => $_POST['summary']
+  ];
+var_dump($review);
+
+  // データベースに接続
+  // ユーザーのポストを取得
+  // ヴァリデーション
+  // データベースに登録
+  // データベースを切断する
+};
