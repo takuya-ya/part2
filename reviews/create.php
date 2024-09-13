fac220d91de15c13e4ddd2baa466d066ddeab9b8
takuya <cdwbh564@yahoo.co.jp>
2024-09-13 17:01:14 +0900

    validationで入力に誤りがある場合、エラー内容を表示して、そのまま新しく入力する画面を表示する処理を追加 エラーが発生しても、レビュー一覧に遷移して、ユーザーエラー内容を確認出来ない為。

--- reviews/create.php
+++ reviews/create.php
@@ -6,7 +6,7 @@ function validation($review){
     $errors = [];
 // if文で入力検査してerrorsに入れる
     if (!strlen($review['title'])) {
-        $errors['title'] = 'タイトルを記入してください';
+        $errors['title'] = 'タイトルを記入してください' . PHP_EOL;
     }
     return $errors;
 }
@@ -59,3 +59,63 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     var_dump($errors);
     // エラーがあれば、new.phpの登録フォールを再表示して、エラーも表示させる
 }
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>読書ログ</title>
+</head>
+
+<body>
+  <h1>読書ログ</h1>
+  <h2>読書ログの登録</h2>
+  <form action="create.php" method="POST">
+        <?php if (count($errors)): ?>
+            <ul>
+                <?php foreach ($errors as $error): ?>
+                    <li>
+                        <?php echo $error; ?>
+                    </li>
+                <?php endforeach; ?>
+            </ul>
+        <?php endif; ?>
+      <div>
+          <label for="title">書籍名</label>
+          <input type="text" id="title" name="title">
+      </div>
+      <div>
+          <label for="author">著者名</label>
+          <input type="text" id="author" name="author">
+      </div>
+      <div>
+          <label for="status">読書状況</label>
+          <div>
+              <div>
+                  <input type="radio" id="status1" name="status" value="未読">
+                  <label for="status1">未読</label>
+              </div>
+              <div>
+                  <input type="radio" id="status2" name="status" value="読書中">
+                  <label for="status2">読書中</label>
+              </div>
+              <div>
+                  <input type="radio" id="status3" name="status" value="読了">
+                  <label for="status3">読了</label>
+              </div>
+          </div>
+      </div>
+      <div>
+          <label for="score">評価（5点満点の整数 ）</label>
+          <input type="text" id="score" name="score">
+      </div>
+      <div>
+          <label for="summary">感想</label>
+          <textarea name="summary" id="summary" rows="10"></textarea>
+      </div>
+      <button type="submit">登録する</button>
+  </form>
+</body>
+</html>
