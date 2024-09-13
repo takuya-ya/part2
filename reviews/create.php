b6cebe7afce43ec81f018f05d0f5f33477ed0fec
takuya <cdwbh564@yahoo.co.jp>
2024-09-13 14:46:29 +0900

    createReview関数の追加、validation処理の途中まで 関数化されていなかったデータ登録の処理を関数化した

--- reviews/create.php
+++ reviews/create.php
@@ -2,44 +2,55 @@
 
 require_once __DIR__ . '/lib/mysqli.php';
 
+function validation($review){
+    $errors = [];
+
+    if ($errors >= 1) {
+
+    }
+    return $errors;
+}
+
+function createReview($link, $review) {
+    $sql = <<<EOT
+    INSERT INTO reviews (
+        title,
+        author,
+        status,
+        score,
+        summary
+    ) VALUES (
+        "{$review['title']}",
+        "{$review['author']}",
+        "{$review['status']}",
+        "{$review['score']}",
+        "{$review['summary']}"
+    )
+EOT;
+
+    $result = mysqli_query($link, $sql);
+    if (!$result) {
+        error_log('Failed to register data.' . PHP_EOL);
+        error_log('Debugging error:' . mysqli_error($link));
+    }
+}
+
 $link = dbConnect();
 $error = mysqli_connect_error($link);
-// HTTPメソッドがPOSTか判定
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     $review = [
         'title' => $_POST['title'],
         'author' => $_POST['author'],
         'status' => $_POST['status'],
-        'score' => $_POST['score'],
+        'score' => (int)$_POST['score'],
         'summary' => $_POST['summary']
     ];
 
-  // ヴァリデーション
-  // データベースに登録
-    $sql = <<<EOT
-        INSERT INTO reviews (
-            title,
-            author,
-            status,
-            score,
-            summary
-        ) VALUES (
-            "{$review['title']}",
-            "{$review['author']}",
-            "{$review['status']}",
-            "{$review['score']}",
-            "{$review['summary']}"
-        )
-    EOT;
 
-$result = mysqli_query($link, $sql);
-if (!$result) {
-    error_log('Failed to register data.' . PHP_EOL);
-    error_log('Debugging error:' . mysqli_error($link));
-}
-
-mysqli_close($link);
-echo 'データベースを切断しました';
+    // $validated = validation($review);
+    createReview($link, $review);
+    mysqli_close($link);
+    echo 'データベースを切断しました';
 }
 header("Location: index.php");
